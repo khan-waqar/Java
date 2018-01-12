808fb0a1d5d2d7852332da53823ed685e0d307e3
ShubhamK <shubhamkhaparey47@gmail.com>
2018-01-12 21:05:10 +0530

    Added s.next()

--- test.java
+++ test.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 class test{
         public static void main(String a[]){
                 Scanner s = new Scanner(System.in);
-                System.out.println(s.next());
+                System.out.println(s.next()+" "+s.next());
         }
 }
