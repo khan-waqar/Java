fc241c1f305c8b014aa588822293e4477c157b59
ShubhamK <shubhamkhaparey47@gmail.com>
2018-01-12 21:02:52 +0530

    Initial commit

---
+++ test.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class test{
+        public static void main(String a[]){
+                Scanner s = new Scanner(System.in);
+                System.out.println(s.next());
+        }
+}
